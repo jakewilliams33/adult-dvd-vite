07473a0b045c79d00ff88b123ab43134f1b3e2e9
jakewilliams33 <jakewilliams33@hotmail.com>
2024-08-21 02:59:28 +0100

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import markdownPlugin from "vite-plugin-markdown";
 import path from "path";
 import serveStatic from "serve-static";
-import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
