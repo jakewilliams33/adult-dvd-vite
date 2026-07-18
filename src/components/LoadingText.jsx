53178344c205f54f205f0b5006217bbe4aaeedcc
jakewilliams33 <83966078+jakewilliams33@users.noreply.github.com>
2026-07-18 12:52:08 +0100

    Update LoadingText.jsx

--- src/components/LoadingText.jsx
+++ src/components/LoadingText.jsx
@@ -3,14 +3,14 @@ import cdb from "../images/cdb.webp";
 export const LoadingText = () => {
   return (
     <>
-      <div className="loading-container">
+      <div data-nosnippet className="loading-container">
         <div className="cd-spinner">
           <div className="side-view"></div>
           <div className="image-container">
             <img className="ringed-image-a" src={cdb} alt="Loading" />
           </div>
         </div>
-        <p data-nosnippet className="loading-text">
+        <p className="loading-text">
           loading <span className="dot dot1">.</span>
           <span className="dot dot2">.</span>
           <span className="dot dot3">.</span>
